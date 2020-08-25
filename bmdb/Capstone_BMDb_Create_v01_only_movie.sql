78b864e480ec2250e2f8e1bbaeed91e1ea08607a
Sean Blessing <snblessing@gmail.com>
2020-08-25 15:52:36 -0400

    reverted only-movie script to original state, added 3 table script

--- bmdb/Capstone_BMDb_Create_v01_only_movie.sql
+++ bmdb/Capstone_BMDb_Create_v01_only_movie.sql
@@ -14,28 +14,6 @@ Director 	varchar(255) 	not null,
 CONSTRAINT unq_movie unique (Title, Year)
 );
 
--- create Actor table
-Create table Actor (
-ID 			integer 		primary key auto_increment,
-FirstName 	varchar(255) 	not null,
-LastName 	varchar(255) 	not null,
-Gender 		varchar(1) 		not null,
-BirthDate 	date 			not null,
-CONSTRAINT unq_actor unique (FirstName, LastName, BirthDate)
-);
-
--- create Credits table
--- business rule - combo of actor+movie must be unique
-Create table Credit (
-ID 				integer 			primary key auto_increment,
-ActorID 		integer 			not null,
-MovieID 		integer 			not null,
-Role		 	varchar(255)		not null,
-Foreign Key (ActorID) references Actor(ID),
-Foreign Key (MovieID) references Movie(ID),
-CONSTRAINT act_mov unique (ActorID, MovieID)
-);
-
 -- Add some movies
  insert into Movie VALUES
  	(1, 'Star Wars', 1977, 'PG', 'George Lucas'),
@@ -43,21 +21,4 @@ CONSTRAINT act_mov unique (ActorID, MovieID)
   	(3, 'Fifth Element', 1997, 'PG-13', 'Luc Besson'),
  	(4, 'Better Off Dead', 1985, 'PG', 'Savage Steve Holland'),
     (5, 'Blazing Saddles', 1974, 'R', 'Mel Brooks')
-    ;
-    
--- Add some actors
--- date fields are YYYY-MM-DD
- insert into Actor VALUES
- 	(1, 'Mark', 'Hamill', 'M', '1951-09-25'),
- 	(2, 'Harrison', 'Ford', 'M', '1942-07-13'),
-    (3, 'Molly', 'Ringwald', 'F', '1968-02-18'),
-    (4, 'Anthony Michael', 'Hall', 'M', '1968-04-14')
-    ;
-
--- Add some credits
- insert into Credit VALUES
- 	(1, 1, 1, 'Luke Skywalker'),
- 	(2, 2, 1, 'Han Solo'),
-    (3, 3, 2, 'Samantha Baker'),
-    (4, 4, 2, 'Geek')
     ;
